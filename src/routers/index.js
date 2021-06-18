8f642854844895905fe916f139344f0385789663
SoftwareOver <71662392+SoftwareOver@users.noreply.github.com>
2021-06-17 22:10:09 -0500

    commit 4
    commit numero 4

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,9 +5,6 @@ const conexion = require('../controllers/conexion');
 module.exports  = function(){
     router.get('/', conexion.inicio);
     router.post('/registro', conexion.confirmacion);
-    router.get('/data-ardruino', conexion.dataArdruino);
-    router.post('/calibrar-alarma', conexion.calibrarAlarma);
-    router.post('/activar-alarma', conexion.activarAlarma, conexion.enviarAlerta);
 
     return router;
 }
