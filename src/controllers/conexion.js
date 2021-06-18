b7b8b611abc081b84bb02f69dc85dea5a4c0e45e
SoftwareOver <71662392+SoftwareOver@users.noreply.github.com>
2021-06-17 23:46:55 -0500

    commit 10
    commit 10

--- src/controllers/conexion.js
+++ src/controllers/conexion.js
@@ -1,4 +1,13 @@
 const nodemailer = require("nodemailer");
+const admin = require('firebase-admin');
+
+//      conexion base de datos
+var serviceAccount = require("../../tecnologias-emergentes-13454-firebase-adminsdk-kqh6g-efaaaf58cd.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://tecnologias-emergentes-13454-default-rtdb.firebaseio.com/'
+});
+const db = admin.firestore();
 
 
 exports.inicio = (req, res, next) => {
@@ -46,4 +55,97 @@ exports.confirmacion = async (req, res, next) => {
     } else {
         res.send({"success": false, "type": "Error en autentificacion", "error": error});
     }
+}
+
+exports.registrar = async (req, res, next) => {
+
+    const respuesta = await db.collection('usuario').add({
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+        codigo: req.body.codigo,
+        distancia: "",
+        estado: false,
+        estadoCasa: false,
+        password: req.body.password,
+        ubicacion: {
+            lat: req.body.ubicacion.lat,
+            long: req.body.ubicacion.long
+        }
+    }).catch( error => {
+        console.log({"error": error});
+        res.send({"success": false, "type": "Error al registara usuario"});
+    })
+
+    
+    console.log("termiono");
+    console.log('Added document with ID: ', respuesta.id);
+    res.send({"success": true, "type": "usuario registrado", data: {"id": respuesta.id}});
+        
+
+}
+
+exports.login = async (req, res, next) => {
+    console.log("entro");
+
+    const citiesRef = db.collection('usuario');
+    const snapshot = await citiesRef.where('correo', '==', req.body.correo).get();
+    if (snapshot.empty) {
+        res.send({"success": false, "type": "Correo no registrado"});
+        return;
+    }
+
+    snapshot.forEach(doc => {
+        res.send({"success": true, "data": doc.data()});
+        console.log(doc.id, '=>', doc.data());
+    });
+
+}
+
+exports.insertarConfirmarcion = async (req, res, next) => {
+    console.log(req.body);
+    var codigo;
+
+    const citiesRef = db.collection('usuario');
+    const snapshot = await citiesRef.where('correo', '==', req.body.correo).get();
+    if (snapshot.empty) {
+        res.send({"success": false, "type": "Correo no registrado"});
+        return;
+    }
+
+    snapshot.forEach(doc => {
+        codigo = doc.data().codigo
+    });
+
+    if (codigo == req.body.codigo) {
+        const userRef = db.collection('usuario').doc(req.body.id);
+
+        userRef.update({
+            codigo: false
+        }).then( () => {
+            console.log("termiono");
+            res.send({"success": true, "type": "Codigo correcto"});
+        }).catch( error => {
+            console.log({"error": error});
+            res.send({"success": false, "type": "Error al confirmar codigo"});
+        })
+    } else {
+        res.send({"success": false, "type": "Codigo incorrecto"});
+    }
+}
+
+exports.general = async (req, res, next) => {
+    const citiesRef = db.collection('usuario');
+    const snapshot = await citiesRef.where('correo', '==', req.body.correo).get();
+    if (snapshot.empty) {
+        res.send({"success": false, "type": "Correo no registrado"});
+        return;
+    }
+
+    snapshot.forEach(doc => {
+        res.send({"success": true, "data": {
+            estadoAlarma: doc.data().estado,
+            estadoCasa: doc.data().estadoCasa
+        }});
+        console.log(doc.id, '=>', doc.data());
+    });
 }
--- src/routers/index.js
+++ src/routers/index.js
@@ -4,7 +4,11 @@ const conexion = require('../controllers/conexion');
 
 module.exports  = function(){
     router.get('/', conexion.inicio);
-    router.post('/registro', conexion.confirmacion);
+    router.post('/registro-codigo', conexion.confirmacion);
+    router.post('/registro', conexion.registrar);
+    router.get('/login', conexion.login);
+    router.put('/registrar-codigo', conexion.insertarConfirmarcion);
+    router.get('/general', conexion.general);
 
     return router;
 }
